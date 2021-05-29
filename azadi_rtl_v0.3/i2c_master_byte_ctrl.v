b3151cbeb683d09e91b7fb1868c0390f09287fab
sajjadahmed677 <merledu677@gmail.com>
2021-05-30 03:49:36 +0500

    synopsis enum_state comment warning resolved

--- azadi_rtl_v0.3/i2c_master_byte_ctrl.v
+++ azadi_rtl_v0.3/i2c_master_byte_ctrl.v
@@ -105,7 +105,7 @@ module i2c_master_byte_ctrl (
 	output [7:0] dout;
 
 	// I2C signals
-	input  scl_i;`include "i2c_master_defines.v"
+	input  scl_i;
 	output scl_o;
 	output scl_oen;
 	input  sda_i;
