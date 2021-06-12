51602a7679d27e6563ab94d77b1b8dadb4b47dcf
zeeshanrafique23 <zeeshanrafique23@gmail.com>
2021-06-12 21:42:59 +0500

    added files for apr

--- yosys_flow/azadi-openlane/config.tcl
+++ yosys_flow/azadi-openlane/config.tcl
@@ -2,8 +2,10 @@
 set ::env(DESIGN_NAME) azadi_soc_top
 
 # Change if needed
-set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/azadi_soc_top_conv.v]
-#/home/merl-tools/openlane-v0.15/openlane/designs/azadi-verilog
+set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/azadi_soc_top_conv-dummy.v]
+
+# Synthesis
+set ::env(SYNTH_NO_FLAT) 1
 
 set ::env(FP_CORE_UTIL) 35
 set ::env(FP_PDN_VOFFSET) 0
@@ -12,15 +14,15 @@ set ::env(FP_PDN_VPITCH) 30
 # For manual macro placement of sram
 #set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/azadi_soc_top_conv_memBB.v]
 #set ::env(MACRO_PLACEMENT_CFG) $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/macro_placement.cfg
-#set ::env(EXTRA_LEFS) [glob $::env(DESIGN_DIR)/macros/lef/*.lef]
-#set ::env(EXTRA_GDS_FILES) [glob $::env(DESIGN_DIR)/macros/gds/*.gds]
+set ::env(EXTRA_LEFS) [glob $::env(DESIGN_DIR)/macros/lef/*.lef]
+set ::env(EXTRA_GDS_FILES) [glob $::env(DESIGN_DIR)/macros/gds/*.gds]
 
 set ::env(PL_TARGET_DENSITY) 0.35
 set ::env(PL_BASIC_PLACEMENT) 1
 set ::env(CELL_PAD) 0
 
 # Fill this
-set ::env(CLOCK_PERIOD) "10"
+set ::env(CLOCK_PERIOD) "40"
 set ::env(CLOCK_PORT) "clk_i"
 set ::env(SYNTH_READ_BLACKBOX_LIB) 1
 
