ba0e7465a659b0d5b50fa9d017e857c766bfc453
sajjadahmed677 <merledu677@gmail.com>
2021-06-16 21:11:44 +0500

    rtl updated to v0.10

--- azadi_rtl_v0.10/rstmgr.sv
+++ azadi_rtl_v0.10/rstmgr.sv
@@ -21,27 +21,26 @@ always_comb begin : comb_part
   // default values
   rst_fsm_ns  = rst_fsm_cs;
   sys_rst_ni  = 1'b0;
-  rst_run_d   = rst_run_q; 
 
   unique case (rst_fsm_cs)
     RESET: begin
       sys_rst_ni   = 1'b0;
-      if (rst_run_q) begin 
-        rst_fsm_ns   = RUN;
-      end else begin
-        rst_fsm_ns   = IDLE;
-      end
+      rst_fsm_ns   = IDLE;
     end
     IDLE: begin
       sys_rst_ni   = 1'b0;
-      if (!prog_rst_ni) begin
+      rst_run_d    = 1'b0;
+      if (rst_run_q) begin 
+        rst_fsm_ns   = RUN;
+      end else if (!prog_rst_ni) begin
         rst_fsm_ns = PROG;
       end else begin
         rst_fsm_ns = IDLE;
       end
     end
     PROG: begin
-      sys_rst_ni       = 1'b0;
+      sys_rst_ni  = 1'b0;
+      rst_run_d   = 1'b0;
       if (!prog_rst_ni) begin
         rst_fsm_ns = PROG;
       end else begin
@@ -50,11 +49,14 @@ always_comb begin : comb_part
     end
     RUN: begin
       sys_rst_ni  = 1'b1;
-      rst_run_d   = 1'b1;
-      if (!prog_rst_ni) begin
-        rst_fsm_ns = PROG;
+      rst_run_d   = 1'b0;
+      if (!rst_ni) begin
+	rst_run_d   = 1'b1;
+	rst_fsm_ns  = RESET;
+      end else if (!prog_rst_ni) begin
+	rst_fsm_ns  = PROG;
       end else begin
-        rst_fsm_ns = RUN;
+	rst_fsm_ns  = RUN;
       end
     end
     default: begin
@@ -74,6 +76,5 @@ always_ff @(posedge clk_i or negedge rst_ni) begin : seq_part
     rst_run_q   <= rst_run_d;
   end
 end
-
 
 endmodule
